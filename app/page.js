93b3ca9562c03ae3c538137d04a1e9b0cb6c7f15
thang <thangcom565@gmail.com>
2025-04-20 01:26:31 +0700

    tạo môi trường và làm landing page

---
+++ app/page.js
@@ -0,0 +1,38 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { ethers } from 'ethers'
+
+// Components
+import Header from "./components/Header"
+// import List from "./components/List"
+// import Token from "./components/Token"
+// import Trade from "./components/Trade"
+
+// // ABIs & Config
+// import Factory from "./abis/Factory.json"
+// import config from "./config.json"
+// import images from "./images.json"
+
+export default function Home() {
+
+  const [provider, setProvider] = useState(null)
+  const [account, setAccount] = useState(null)
+  
+  async function loadBlockchainData() {
+    const provider = new ethers.BrowserProvider(window.ethereum)
+    setProvider(provider)
+
+  }
+
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+  
+  return (
+    <div className="page">
+      <Header account={account} setAccount={setAccount} />
+    </div>
+  );
+}
